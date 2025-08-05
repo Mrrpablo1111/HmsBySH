567d3926a444b8f505e8d49d4a1d6a5dff10ac23
Seng Hong <hongseng586@gmail.com>
2025-08-05 10:10:42 +0700

    Add Eureka

---
+++ Backend/Eureka-Server/src/test/java/com/hms/eureka_server/Eureka_Server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.eureka_server.Eureka_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
